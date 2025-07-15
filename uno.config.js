07f6e08a2c841111456c30c00abf437f2ca6ac62
Eltrac <hi@guhub.cn>
2025-07-15 11:40:37 +0800

    refactor: replace _fictions.scss with unocss

--- uno.config.js
+++ uno.config.js
@@ -1,9 +1,37 @@
-import presetWind4 from '@unocss/preset-wind4'
-import { defineConfig } from 'unocss'
+import { defineConfig, presetWind3, presetAttributify } from 'unocss'
 
 export default defineConfig({
+  safelist: [
+      'text-sm',
+      'text-md',
+      'text-lg',
+      'text-xl',
+      'text-2xl',
+      'text-3xl',
+      'text-4xl',
+      'text-5xl',
+
+      // rotate -4 to +4 deg
+      'rotate-[-3.0deg]',
+      'rotate-[-2.0deg]',
+      'rotate-[-1.0deg]',
+      'rotate-[0.0deg]',
+      'rotate-[1.0deg]',
+      'rotate-[2.0deg]',
+      'rotate-[3.0deg]',
+
+      // opacity from 0.6 to 1.0
+      'opacity-[0.6]',
+      'opacity-[0.7]',
+      'opacity-[0.8]',
+      'opacity-[0.9]',
+      'opacity-[1.0]',
+    ],
   content: {
     filesystem: ['./**/*.{html,md,js}'],
   },
-  presets: [presetWind4()],
+  presets: [
+    presetAttributify(),
+    presetWind3(),
+  ],
 })
